4f16596414f2cdaed6708abd87685b258d8db1d1
Nathan Daly <nathand@openobjects.com>
2017-07-03 17:03:07 +0100

    Added ThemeProvider example.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
 import Wrapper from './components/Wrapper';
 import Button from './components/Button';
+import MaxTheme from './constants/MaxTheme'
 import './App.css';
 
 class App extends Component {
   render() {
   return (
     <div>
-      <Wrapper green>
-        <Button>Normal</Button>
-        <Button type='primary'>Primary</Button>
-        <Button type='sexy'>Sexy</Button>
-      </Wrapper>
+      <ThemeProvider theme={MaxTheme}>
+        <Wrapper green>
+          <Button>Normal</Button>
+          <Button type='primary'>Primary</Button>
+          <Button type='sexy'>Sexy</Button>
+        </Wrapper>
+      </ThemeProvider>
     </div>
   );
   }
---
+++ src/constants/MaxTheme.js
@@ -0,0 +1,36 @@
+export const MaxTheme = {
+      max: {
+        Button: {
+          default: {
+            background: '#d3d3d3',
+            color: 'white'
+          },
+          primary: {
+            background: 'blue',
+            color: 'white'
+          },
+          success: {
+            background: 'green',
+            color: 'white'
+          },
+          info: {
+            background: 'lightblue',
+            color: 'white'
+          },
+          warning: {
+            background: 'yellow',
+            color: 'white'
+          },
+          danger: {
+            background: 'lightblue',
+            color: 'orange'
+          },
+          sexy: {
+            background: 'hotpink',
+            color: 'white'
+          }
+        }
+      }
+    };
+
+export default MaxTheme;    
